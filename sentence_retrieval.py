298340f7b0bb713098814a791902576221f759f1
Aniketh Reddy <anikethjr@gmail.com>
2018-06-08 14:02:01 +0530

    Finished NER based sentence retrieval

---
+++ sentence_retrieval.py
@@ -0,0 +1,29 @@
+import os
+import jsonlines
+import nltk
+import codecs
+import utilities
+
+def getRelevantSentences(relevant_docs, wiki_split_docs_dir):
+	entities = []
+	split_entities = []
+	relevant_sentences = []
+	for relevant_doc in relevant_docs:
+		relevant_doc = relevant_doc.replace("-SLH-","/")
+		relevant_doc = relevant_doc.replace("_"," ")
+		entities.append(relevant_doc)
+		split_entities = split_entities + relevant_doc.split(" ")
+	for relevant_doc in relevant_docs:
+		file = codecs.open(wiki_split_docs_dir + "/" + relevant_doc + ".txt","r","utf-8")
+		lines = file.readlines()
+		for i in range(len(lines)):
+			lines[i] = lines[i].strip()
+			split_line = lines[i].split(" ")
+			intersection = utilities.listIntersection(split_line,split_entities)
+			if len(intersection) > 0:
+				temp = {}
+				temp['id'] = relevant_doc
+				temp['line_num'] = i+1
+				temp['sentence'] = lines[i]
+				relevant_sentences.append(temp)
+	return relevant_sentences
---
+++ utilities.py
@@ -0,0 +1,25 @@
+import os
+import jsonlines
+import nltk
+import codecs
+
+def getNamedEntities(sentence):
+	# print sentence
+	# print nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sentence)))
+	temp = codecs.open("temp.txt","w+","utf-8")
+	temp.write(sentence)
+	temp.close()
+	cmd = "nohup java -mx600m -cp ner/stanford-ner/stanford-ner.jar:ner/stanford-ner/lib/* edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner/stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz -outputFormat tabbedEntities -textFile temp.txt > temp.tsv"
+	os.system(cmd)
+	temp = codecs.open("temp.tsv","r","utf-8").readlines()
+	entities = []
+	for line in temp:
+		line = line.strip().split("\t")
+		if len(line)>1:
+			ent = line[0]
+			entities.append(ent)
+	return entities
+
+def listIntersection(list1,list2):
+	s = set(list2)
+	return [val for val in list1 if val in s]
