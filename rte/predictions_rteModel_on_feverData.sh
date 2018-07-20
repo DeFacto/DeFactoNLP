bc2d9e71354b323264ad3fd8f6a056503821557d
Gil Rocha <rocha@ukp.informatik.tu-darmstadt.de>
2018-07-20 13:18:51 +0100

    configuration file: path fixed

--- rte/predictions_rteModel_on_feverData.sh
+++ rte/predictions_rteModel_on_feverData.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m allennlp.run predict ../fever_data/model.tar.gz  ./fever_data/test_fever_snliFormat.jsonl --output-file predictions_rteModel_on_feverData.json --silent
+python -m allennlp.run predict snli_output/model.tar.gz  ./fever_data/test_fever_snliFormat.jsonl --output-file predictions_rteModel_on_feverData.json --silent
