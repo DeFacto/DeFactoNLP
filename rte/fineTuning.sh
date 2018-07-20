4e94513cbd6dd38a8f6f248c768aa720727403ee
Gil Rocha <rocha@ukp.informatik.tu-darmstadt.de>
2018-07-20 13:02:37 +0100

    bug fix

--- rte/fineTuning.sh
+++ rte/fineTuning.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python -m allennlp.run fine-tune --model-archive ./snli_output/model.tar.gz --config-file ./fineTuning_config/decomposable_attention_fineTuning.json  --serialization-dir ./fever_output/  --file-friendly-logging True
+python -m allennlp.run fine-tune --model-archive ./snli_output/model.tar.gz --config-file ./fineTuning_config/decomposable_attention_fineTuning.json  --serialization-dir ./fever_output/
 #TODO: check if "--overrides" parameter is relevant
