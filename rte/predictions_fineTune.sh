1f65f8e326eb3d5122fde7b8e670557878da4ef9
GilRocha <ei11124@fe.up.pt>
2018-08-09 16:00:49 +0100

    Update predictions_fineTuning.sh for FEVER SNLI-style test set

--- rte/predictions_fineTune.sh
+++ rte/predictions_fineTune.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m allennlp.run predict ./fever_output/model.tar.gz  ./fever_data/test_fever_snliFormat.jsonl --output-file predictions_rte_fever.json --silent
+python -m allennlp.run predict ./fever_output/model.tar.gz  ./fever_data/test_fever_snliFormat.jsonl --output-file predictions_rte_fever_testSet.json --silent
