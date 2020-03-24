3434d4b3bba777923b66f1d22a7ef40a0f05d901
pedrojlazevedo <up201306026@fe.up.pt>
2020-03-24 02:04:32 +0000

    Creating Vectors Representations

--- doc2vec.py
+++ doc2vec.py
@@ -3,20 +3,51 @@ import jsonlines
 from random import shuffle
 import gensim
 import sys
+import spacy
+from gensim.parsing.preprocessing import remove_stopwords
+from gensim.models.doc2vec import Doc2Vec
+
+fname = "doc2vec.model"
 
 import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+spacy_nlp = spacy.load('en_core_web_sm')
+spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
+
+customize_stop_words = [
+    "-LRB-", "-RRB-", "-LSB-", "-LRB-"
+]
+
+for w in customize_stop_words:
+    spacy_nlp.vocab[w].is_stop = True
+
 if len(sys.argv) - 1 == 1:
-    max_counter = 1000000  # 1 000 000
+    max_counter = int(sys.argv[1])
 else:
     max_counter = 10000  # 10 000
     print("Max Counter not defined!")
     print("Set Default Value: " + str(max_counter))
 
+
+def pre_process(doc):
+    # doc = spacy_nlp(doc)
+
+    # lemma_tokens = [token.lemma_ for token in doc] 
+    # doc = ' '.join(map(str, lemma_tokens))
+    # doc = spacy_nlp(doc)
+
+    # tokens = [token.text for token in doc if not token.is_stop]
+
+    # text = ' '.join(map(str, tokens))
+    text = remove_stopwords(doc)
+    return text
+
+# TODO:Remove all STOP-WORDS and Lemmatize every token!!!!!
+
 # full text and processed in ['text'] tag
-wiki_folder = "../wiki-pages-split"
+wiki_folder = "data/wiki-pages-split"
 files = os.listdir(wiki_folder)
 shuffle(files)
 
@@ -26,9 +57,24 @@ train_text = []
 tokens = []
 for file in files:
     file_content = jsonlines.open(wiki_folder + "/" + file)
-    file_content = file_content.read()
-    text = file_content['text']
+    doc = file_content.read()['text']
+    text = pre_process(doc)
+
     if counter > max_counter:
+        # adding required docs by fever with the claim given
+        file_content = jsonlines.open(wiki_folder + "/" + "Telemundo.json")
+        doc = file_content.read()['text']
+        text = pre_process(doc)
+        tokens = gensim.utils.simple_preprocess(text)
+        print(tokens)
+        train_text.append(gensim.models.doc2vec.TaggedDocument(tokens, ["Telemundo.json"]))
+
+        file_content = jsonlines.open(wiki_folder + "/" + "Hispanic_and_Latino_Americans.json")
+        doc = file_content.read()['text']
+        text = pre_process(doc)
+        tokens = gensim.utils.simple_preprocess(text)
+        train_text.append(gensim.models.doc2vec.TaggedDocument(tokens, ["Hispanic_and_Latino_Americans.json"]))
+
         break
     else:
         tokens = gensim.utils.simple_preprocess(text)
@@ -37,23 +83,55 @@ for file in files:
         if counter % 1000 == 0:
             print(counter)
 
-model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=20, epochs=2)
-model.build_vocab(train_text)
+model = Doc2Vec(vector_size=50, min_count=2, epochs=40)
+#model = Doc2Vec.load(fname)
+model.build_vocab(train_text)#,keep_raw_vocab=True)#, update=True)
 
 model.train(train_text, total_examples=model.corpus_count, epochs=model.epochs)
 
-sentence = "Obama was president of United States of America similar to a Portuguese person called D. Afonso Henriques"
-test_sentence = gensim.utils.simple_preprocess(sentence)
-inferred_vector = model.infer_vector(test_sentence)
+sentence = "Telemundo is a English-language television network."
+text = pre_process(sentence)
+tokens = gensim.utils.simple_preprocess(text)
+print(tokens)
+for token in tokens:
+    print(token)
+    inferred_vector = model.infer_vector([token])
+    sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+
+    STOP = 3
+    for doc, sim in sims:
+        file_content = jsonlines.open(wiki_folder + "/" + doc)
+        file_content = file_content.read()
+        text = file_content['text']
+        print("\n" + doc + " -- " + str(sim) + ": \n")  # + text)
+        if STOP == 0:
+            break
+        else:
+            STOP -= 1
+
+    for doc, sim in sims:
+        if doc != "Hispanic_and_Latino_Americans.json" and doc != "Telemundo.json":
+            continue
+        print(doc + " -- " + str(sim))
+    print("\n")
+
+model.save(fname)
+
+inferred_vector = model.infer_vector(tokens)
 sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
 
-STOP = 5
+STOP = 3
 for doc, sim in sims:
     file_content = jsonlines.open(wiki_folder + "/" + doc)
     file_content = file_content.read()
     text = file_content['text']
-    print("\n" + doc + " -- " + str(sim) + ": \n" + text)
+    print("\n" + doc + " -- " + str(sim) + ": \n")  # + text)
     if STOP == 0:
         break
     else:
         STOP -= 1
+
+for doc, sim in sims:
+    if doc != "Hispanic_and_Latino_Americans.json" and doc != "Telemundo.json":
+        continue
+    print(doc + " -- " + str(sim))
---
+++ word2vec.py
@@ -0,0 +1,186 @@
+import os
+import jsonlines
+from random import shuffle
+import gensim
+import sys
+from scipy import spatial
+import spacy
+import numpy as np
+
+fname = "word2vec.model"
+
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+spacy_nlp = spacy.load('en_core_web_sm')
+
+if len(sys.argv) - 1 == 1:
+    max_counter = int(sys.argv[1])
+    if max_counter == -1:
+        max_counter = 9999999
+else:
+    max_counter = 10000  # 10 000
+    print("Max Counter not defined!")
+    print("Set Default Value: " + str(max_counter))
+
+# full text and processed in ['text'] tag
+wiki_folder = "data/wiki-pages-split"
+files = os.listdir(wiki_folder)
+shuffle(files)
+
+counter = 0
+
+train_sentences = []
+tokens = []
+for file in files:
+    _name = file[:-5]
+    title = _name.replace("_", " ")
+    tokens = gensim.utils.simple_preprocess(title)
+    train_sentences.append(tokens)
+
+    file_content = jsonlines.open(wiki_folder + "/" + file)
+    lines = file_content.read()['lines']
+    for line in lines:
+        if len(line['content']) < 2:
+            continue
+        tokens = gensim.utils.simple_preprocess(line['content'])
+        train_sentences.append(tokens)
+    if counter > max_counter:
+        # adding required docs by fever with the claim given
+        file_content = jsonlines.open(wiki_folder + "/" + "Telemundo.json")
+        lines = file_content.read()['lines']
+        for line in lines:
+            print(line['content'])
+            tokens = gensim.utils.simple_preprocess(line['content'])
+            train_sentences.append(tokens)
+
+        file_content = jsonlines.open(wiki_folder + "/" + "Hispanic_and_Latino_Americans.json")
+        lines = file_content.read()['lines']
+        for line in lines:
+            print(line['content'])
+            tokens = gensim.utils.simple_preprocess(line['content'])
+            train_sentences.append(tokens)
+
+        break
+    else:
+        counter += 1
+        if counter % 1000 == 0:
+            print(counter)
+
+model = gensim.models.Word2Vec(iter=1, min_count=5, size=500, workers=4)  # an empty model, no training yet
+model.build_vocab(train_sentences)  # can be a non-repeatable, 1-pass generator
+
+print(model.epochs)
+model.train(train_sentences, total_examples=model.corpus_count, epochs=30)  # can be a non-repeatable, 1-pass generator
+index2word_set = set(model.wv.index2word)
+
+
+def avg_feature_vector(sentence, model, num_features, index2word_set):
+    words = sentence.split()
+    feature_vec = np.zeros((num_features,), dtype='float32')
+    n_words = 0
+    for word in words:
+        if word in index2word_set:
+            n_words += 1
+            feature_vec = np.add(feature_vec, model[word])
+    if n_words > 0:
+        feature_vec = np.divide(feature_vec, n_words)
+    return feature_vec
+
+
+def word2vec(text1, text2):
+    # print(text1)
+    # print(text2)
+    s1_afv = avg_feature_vector(text1, model=model, num_features=500, index2word_set=index2word_set)
+    s2_afv = avg_feature_vector(text2, model=model, num_features=500, index2word_set=index2word_set)
+    if np.sum(s1_afv) == 0 or np.sum(s2_afv) == 0:
+        return 0
+        # text1 = spacy_nlp(text1)
+        # text2 = spacy_nlp(text2)
+        # sim = text1.similarity(text2)
+    else:
+        sim = 1 - spatial.distance.cosine(s1_afv, s2_afv)
+
+    return sim
+
+
+sentence = "Telemundo is a English-language television network."
+document = "Telemundo"
+document_2 = "Hispanic_and_Latino_Americans"
+
+print(word2vec("cat", "man"))
+print(word2vec("cat", "dog"))
+#print(model.similarity("cat", "man"))
+#print(model.similarity("cat", "dog"))
+
+tokens_sentence = gensim.utils.simple_preprocess(sentence)
+sentence = ' '.join(map(str, tokens_sentence))
+print(sentence)
+
+best = [0, 0, 0, 0, 0]
+docs = ["", "", "", "", ""]
+for file in files:
+    _name = file[:-5]
+    title = _name.replace("_", " ")
+    tokens = gensim.utils.simple_preprocess(title)
+    text = ' '.join(map(str, tokens))
+    sim = word2vec(sentence, text)
+    if sim > best[0]:
+        best[0] = sim
+        docs[0] = _name
+
+    elif sim > best[1]:
+        best[1] = sim
+        docs[1] = _name
+
+    elif sim > best[2]:
+        best[2] = sim
+        docs[2] = _name
+
+    elif sim > best[3]:
+        best[3] = sim
+        docs[3] = _name
+
+    elif sim > best[4]:
+        best[4] = sim
+        docs[4] = _name
+
+print(best)
+print(docs)
+
+tokens_sentence = gensim.utils.simple_preprocess(sentence)
+
+for token_sentence in tokens_sentence:
+    best = [0, 0, 0, 0, 0]
+    docs = ["", "", "", "", ""]
+    for file in files:
+        _name = file[:-5]
+        title = _name.replace("_", " ")
+        tokens = gensim.utils.simple_preprocess(title)
+        text = ' '.join(map(str, tokens))
+        sim = word2vec(token_sentence, text)
+        if sim > best[0]:
+            best[0] = sim
+            docs[0] = _name
+
+        elif sim > best[1]:
+            best[1] = sim
+            docs[1] = _name
+
+        elif sim > best[2]:
+            best[2] = sim
+            docs[2] = _name
+
+        elif sim > best[3]:
+            best[3] = sim
+            docs[3] = _name
+
+        elif sim > best[4]:
+            best[4] = sim
+            docs[4] = _name
+    print(best)
+    print(docs)
+
+print(word2vec("telemundo", sentence))
+
+model.save('models/word2vec')
