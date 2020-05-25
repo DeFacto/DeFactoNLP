551af86fd317d365ab723e85f7369bf5a663af09
pedrojlazevedo <up201306026@fe.up.pt>
2020-05-25 21:49:52 +0100

    DOcument Retrieval Improvements

---
+++ run_sentence_selection_2.py
@@ -0,0 +1,143 @@
+import jsonlines
+import codecs
+import json
+from sentence_transformers import SentenceTransformer
+import scipy.spatial
+from triple_sentence_selection import get_pairs_from_doc
+from sklearn.metrics.pairwise import cosine_similarity
+
+wiki_split_docs_dir = "../wiki-pages-split"
+relevant_docs_file = "data/subsample_train_test_baby.jsonl"
+relevant_sent_file = "data/subsample_train_transformers_baby.jsonl"
+
+relevant_docs_file = jsonlines.open(relevant_docs_file)
+
+INCLUDE_OIE = True
+
+
+# relevant_sent_file = jsonlines.open(relevant_sent_file)
+
+def get_sentence(doc, line_num):
+    try:
+        file = codecs.open(wiki_split_docs_dir + "/" + doc + ".json", "r", "latin-1")
+    except:
+        print("Failed Loading" + str(doc))
+        return ""
+
+    file = json.load(file)
+    full_lines = file["lines"]
+    lines = []
+    for _line in full_lines:
+        lines.append(_line['content'])
+    _sentence = lines[line_num]
+    return _sentence
+
+
+def clean_sentence(_sentence):
+    _sentence = _sentence.replace("-LRB-", "-LRB- ")
+    _sentence = _sentence.replace("-RRB-", " -RRB-")
+    _sentence = _sentence.replace("-LSB-", "-LSB- ")
+    _sentence = _sentence.replace("-RSB-", " -RSB-")
+    return _sentence
+
+
+if __name__ == '__main__':
+    # embedder = SentenceTransformer('roberta-large-nli-stsb-mean-tokens')
+    embedder = SentenceTransformer('output/bert-base-nli-with-doc-augmented')
+    # embedder = SentenceTransformer('bert-base-nli-mean-tokens')
+
+    claims = []
+    for line in relevant_docs_file:
+        claims.append(line)
+
+    # # testing
+    # claim_0 = claims[0]
+    # for pair in claim_0['predicted_sentences_ner']:
+    #     print("\n")
+    #     print(pair[0])
+    #     print(pair[1])
+    #     print(get_sentence(pair[0], pair[1]))
+
+    STOP = -1
+    with jsonlines.open(relevant_sent_file, mode='w') as writer_c:
+        for claim in claims:
+            # get all possible sentences
+            pair_sent_pair = {}
+
+            if INCLUDE_OIE:
+                for doc in claim['predicted_pages_oie']:
+                    pairs = get_pairs_from_doc(doc)
+                    pairs += claim['predicted_sentences']
+                    for pair in pairs:
+                        sentence = get_sentence(pair[0], pair[1])
+                        # TODO: ALERT!!!! ADDING DOC!!!!!!!!
+                        doc = clean_sentence(pair[0]).replace("_", " ")
+                        sentence = doc + " " + sentence
+                        pair_sent_pair[sentence] = (pair[0], pair[1])
+                    # print(doc)
+                    # print(pairs)
+            else:
+                for pair in claim['predicted_sentences_ner']:
+                    sentence = get_sentence(pair[0], pair[1])
+                    sentence = clean_sentence(sentence)
+                    title = pair[0].replace("_", " ")
+
+                    # if not title.lower() in sentence.lower():
+                    #     sentence = pair[0] + " " + sentence
+                    pair_sent_pair[sentence] = (pair[0], pair[1])
+
+            # for pair in claim['predicted_sentences']:
+            #     sentence = get_sentence(pair[0], pair[1])
+            #     sentence = clean_sentence(sentence)
+            #     pair_sent_pair[sentence] = (pair[0], pair[1])
+
+            corpus = []
+            sentence_identifier = []
+            for key in pair_sent_pair:
+                corpus.append(key)
+                sentence_identifier.append(pair_sent_pair[key])
+
+            claim['predicted_sentences_bert_5'] = []
+            claim['predicted_sentences_bert_10'] = []
+
+            # create embeddings
+            corpus_embeddings = embedder.encode(corpus)
+            query_embeddings = embedder.encode([claim['claim']])
+
+            # get the n most similar sentences
+            closest_n = 5
+            for query, query_embedding in zip([claim['claim']], query_embeddings):
+                distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
+                results = zip(range(len(distances)), distances)
+                results = sorted(results, key=lambda x: x[1], reverse=False)
+
+                print("\n\n======================\n\n")
+                print("Query:", query)
+                print("\nTop 5 most similar sentences in corpus:")
+
+                for idx, distance in results[0:closest_n]:
+                    print(corpus[idx].strip(), "(Score: %.4f)" % (1 - distance))
+                    print(sentence_identifier[idx])
+                    claim['predicted_sentences_bert_5'].append(sentence_identifier[idx])
+
+            # get the n most similar sentences
+            closest_n = 10
+            for query, query_embedding in zip([claim['claim']], query_embeddings):
+                distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
+                results = zip(range(len(distances)), distances)
+                results = sorted(results, key=lambda x: x[1], reverse=False)
+
+                # print("\n\n======================\n\n")
+                # print("Query:", query)
+                # print("\nTop 5 most similar sentences in corpus:")
+
+                for idx, distance in results[0:closest_n]:
+                    # print(corpus[idx].strip(), "(Score: %.4f)" % (1 - distance))
+                    # print(sentence_identifier[idx])
+                    claim['predicted_sentences_bert_10'].append(sentence_identifier[idx])
+            writer_c.write(claim)
+            print(STOP)
+            if STOP == 0:
+                break
+            else:
+                STOP -= 1
---
+++ triple_sentence_selection_tuned.py
@@ -0,0 +1,169 @@
+import jsonlines
+import codecs
+import json
+import numpy as np
+from sklearn.externals import joblib
+from proof_extraction_train import _extract_features
+from defacto.model_nl import ModelNL
+import unicodedata as ud
+
+# Non-CoRef Docs
+# wiki_split_docs_dir = "../wiki-pages-split"
+# CoRef Docs
+wiki_split_docs_dir = "../wiki-pages-coref"
+
+
+def clean_doc(_doc):
+    _doc = _doc.replace("-SLH-", " / ")
+    _doc = _doc.replace("_", " ")
+    _doc = _doc.replace("-LRB-", " ( ")
+    _doc = _doc.replace("-RRB-", " ) ")
+    _doc = _doc.replace("-COLON-", " : ")
+    return _doc
+
+
+def get_file(_doc, _wiki_split_docs_dir=wiki_split_docs_dir):
+    try:
+        _doc = ud.normalize('NFC', _doc)
+        file = codecs.open(_wiki_split_docs_dir + "/" + _doc + ".json", "r", "utf8")
+        file = json.load(file)
+        return file
+    except Exception as _e:
+        print("Failed Loading" + str(_doc) + str(_e))
+        return ""
+
+
+def get_lines(file):
+    full_lines = file["lines"]
+    lines = []
+    for _line in full_lines:
+        lines.append(_line['content'])
+    return lines
+
+
+def get_pairs_from_doc(_doc, _wiki_split_docs_dir=wiki_split_docs_dir):
+    file = get_file(_doc, _wiki_split_docs_dir=_wiki_split_docs_dir)
+    if file == "":
+        return ""
+    lines = get_lines(file)
+    _pairs = []
+    for i in range(len(lines)):
+        if lines[i] != "":
+            _pairs.append((_doc, i))
+    return _pairs
+
+
+def get_sentence(_doc, line_num, _wiki_split_docs_dir=wiki_split_docs_dir):
+    file = get_file(_doc, _wiki_split_docs_dir=_wiki_split_docs_dir)
+    if file == "":
+        return ""
+    lines = get_lines(file)
+    _sentence = lines[line_num]
+    return _sentence
+
+
+if __name__ == "__main__":
+    document_results_file = "data/dev_test_triple_baby.jsonl"  # file with tfidf only
+    # document_results_file = "data/dev_concatenation.jsonl"  # file with tfidf and ner predicted_sentences
+    document_results_file_oie = "data/dev_test_triple_baby.jsonl"  # file with tfidf and oie
+
+    document_results_file = jsonlines.open(document_results_file)
+    document_results_file_oie = jsonlines.open(document_results_file_oie)
+
+    results_file = "data/dev_coref_triple_tuned.jsonl"
+
+    defacto_clf = joblib.load('defacto/defacto_models/rfc.mod')
+
+    claims = []
+    for line in document_results_file:
+        claims.append(line)
+
+    claims_oie = []
+    for line in document_results_file_oie:
+        claims_oie.append(line)
+
+    errors = 0
+    correct = 0
+    no_prediction = 0
+
+    with jsonlines.open(results_file, mode='w') as writer_c:
+        advance_lines = 0
+        for line in claims_oie:
+            advance_lines -= 1
+            if advance_lines > 0:
+                continue
+            correct_sentences = set()
+            correct_probabilities = set()
+            flag = False
+            try:
+                defactoModel = None
+                all_pairs = line['predicted_sentences']
+                all_pairs = [tuple(p) for p in all_pairs]
+                if 'predicted_pages_oie' in line:
+                    documents = line['predicted_pages_oie']
+                    for doc in documents:
+                        pairs = get_pairs_from_doc(doc)
+                        all_pairs.extend(pairs)
+                all_pairs = list(set(all_pairs))
+                for pair in all_pairs:
+                    if defactoModel is None:
+                        defactoModel = ModelNL(claim=line['claim'])
+                    sentence = get_sentence(pair[0], pair[1])
+                    if sentence == "":
+                        continue
+                    try:
+                        x = _extract_features(sentence, line['claim'], defactoModel.triples)
+                        x = np.asarray(x)
+                        x = x.reshape(1, -1)
+                        y = defacto_clf.predict(x)
+                        y_prob = defacto_clf.predict_proba(x)
+                        defacto_class = y[0]
+                    except Exception as e:
+                        errors += 1
+                        print("Error: " + str(errors))
+                    if defacto_class == 0:
+                        continue
+                    else:
+                        correct_sentences.add(pair)
+                        correct_probabilities.add((pair[0], pair[1], y_prob[0][1]))
+            except Exception as e:
+                print("Error")
+                flag = True
+            correct += 1
+            print(correct)
+            if len(correct_sentences) > 0:
+                correct_sentences = list(correct_sentences)
+                line['predicted_sentences_triple'] = correct_sentences
+            else:
+                print("NO PREDICTION!!!!")
+                if flag:
+                    no_prediction += 1
+                line['predicted_sentences_triple'] = line['predicted_sentences']
+
+            # Create exactly 10 sentences for the triple selection
+            size = len(line['predicted_sentences_triple'])
+            ordered_sents = sorted(correct_probabilities, key=lambda tup: tup[2], reverse=True)
+            BREAK_INFINITE_LOOP = 10
+            line['predicted_sentences_triple_all'] = line['predicted_sentences_triple']
+            while size != 10:
+                if size > 10:
+                    line['predicted_sentences_triple'] = []
+                    for i in range(0, 10):
+                        line['predicted_sentences_triple'].append((ordered_sents[i][0], ordered_sents[i][1]))
+                        print(ordered_sents[i][2])
+                elif size < 10:
+                    for pair in all_pairs:
+                        if pair not in line['predicted_sentences_triple'] and pair not in correct_sentences:
+                            line['predicted_sentences_triple'].append(pair)
+                            break
+                BREAK_INFINITE_LOOP -= 1
+                if BREAK_INFINITE_LOOP < 1:
+                    print("INFINITE LOOP DETECTED")
+                    break
+                size = len(line['predicted_sentences_triple'])
+            # for pair in line['predicted_sentences_triple']:
+            #     print(pair)
+            writer_c.write(line)
+    print(no_prediction)
+    print(correct)
+    print(no_prediction)
